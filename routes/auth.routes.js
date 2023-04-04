8fa5d27b37c435101585ac31cbf8b61427b989ef
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-04-04 17:52:25 +0200

    modified dthe request model property to status in validations instead of approval

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -56,7 +56,7 @@ router.post("/create-user", (req, res, next) => { // ADD THE MIDDLE WARE and ver
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name, surname, contractStartDate, isNewEmployee: true, position, companyId  });
+      return User.create({ email, password: hashedPassword, name, surname, contractStartDate,validators, isNewEmployee: true, position, companyId  });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
@@ -121,7 +121,7 @@ router.post("/login", (req, res, next) => {
 router.get("/verify", isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and is made available on `req.payload`
-  console.log(`req.payload`, req.payload);
+  // console.log(`req.payload`, req.payload);
 
   // Send back the token payload object containing the user data
   res.status(200).json(req.payload);
