20d6ed4d6987c7116d7cde8383ab59c85b7cc06b
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-04-06 20:58:48 +0200

    added duration to requestion model

--- routes/request.routes.js
+++ routes/request.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
 const Request = require("../models/Request.model");
+const differenceInBusinessDays = require('date-fns/differenceInBusinessDays')
 
 router.post("/user/create-request", (req, res, next) => {
   const {
@@ -16,18 +17,21 @@ router.post("/user/create-request", (req, res, next) => {
     requester,
     comments,
   } = req.body;
-
+  let duration = differenceInBusinessDays(new Date(endDate), new Date(startDate)) + 1
+  if(morningAfternoonStart === "afternoon") duration -= 0.5
+  if(morningAfternoonEnd === "morning") duration -= 0.5
+  
+  console.log("received approval limite date", approvalLimitDate)
   User.findById(requester)
     .populate("companyId")
     .then((foundUser) => {
       const { validators } = foundUser;
       console.log(validators)
       let validations = []
-      validators.map((validator) => validations.push({validatorId: validator, approval:"pending"}));
-      const daysLeft = foundUser.companyId.numberOfVacationDays; //to be refined with vacation day calculation
-      // add if condition daysleft > demanded days
-      if (true) {
+      validators.map((validator) => validations.push({validatorId: validator, status:"pending"}));
+      
         Request.create({
+          duration,
           status,
           approvalLimitDate,
           isFullDay,
@@ -41,22 +45,27 @@ router.post("/user/create-request", (req, res, next) => {
         })
         .then((request)=> res.json(request))
         .catch((err)=>('err in sending the request', err))
-
-      }
     }) 
     .catch((err) => console.log("error in payload._id", err));
 });
 
 router.get("/requests", (req,res,next)=>{
     Request.find()
-    .populate("requester")
+    .populate("requester validations.validatorId")
     .then(requests=> res.json(requests))
     .catch(err=>console.log("err in retrieving the Request", err))
   })
 
+router.get("/user/:id/requests", (req,res,next)=>{
+    Request.find({requester: req.params.id})
+    .populate("requester validations.validatorId")
+    .then(requests=> res.json(requests))
+    .catch(err=>console.log("err in retrieving the Request", err))
+})
+
 router.get("/request/:id", (req,res,next)=>{
     Request.findById(req.params.id)
-    .populate("requester")
+    .populate("requester validations.validatorId")
     .then(request=> res.json(request))
     .catch(err=>console.log("err in retrieving the Request", err))
   })
@@ -71,6 +80,7 @@ router.put("/request/:id/settings", (req, res, next)=>{
         comments,
         validations,
       } = req.body;
+      
       let status = "pending"
 /*       console.log("validations:",validations) */
       /* status is now treated on the fron end */
